60709731b023926c69e4389bf2aadf2b789e3357
Shreya Mishra <1mishra.palak9696@gmail.com>
2025-07-08 09:47:08 +0530

    Time: 0 ms (100%), Space: 42.9 MB (66.29%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -9,6 +9,8 @@ class Solution {
                 return mid;
             } else if (nums[mid] < target) {
                 left = mid+1;
+            } else {
+                right = mid -1 ;
             }
         }
         return left;
